031cb3775aec56e4b55f7156be1b668f7a609057
Mateo Rodriguez <mateo.rodriguez.fierro@gmail.com>
2023-11-19 18:25:37 -0500

    calculadora inversion banco

--- calculatorapp/views.py
+++ calculatorapp/views.py
@@ -7,18 +7,11 @@ def home(request):
 
 def result(request):
     num1 = int(request.GET.get('number1'))
-    num2 = int(request.GET.get('number2'))
+    percentage = int(request.GET.get('percentage'))
 
 
-    if request.GET.get('add') == "":
-        ans = num1 + num2
-    elif request.GET.get('subtract') == "":
-        ans = num1 - num2
-    elif request.GET.get('multiply') == "":
-        ans = num1 * num2
-    else:
-        ans = num1 / num2
-
+    if request.GET.get('multiply') == "":
+        ans = num1 * percentage / 100
 
     return render(request, 'result.html', {'ans': ans})
 
